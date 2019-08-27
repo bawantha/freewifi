ffa08c322b18f8e716e338c690234f47f8670c70
Bawantha Mihiran Rajapaksha <bawwa.16@cse.mrt.ac.lk>
2019-08-27 12:18:44 +0530

    no state management bu works

---
+++ lib/bloc/dealbloc.dart
@@ -0,0 +1,8 @@
+import 'package:freewifi/models/dealdata.dart';
+import 'package:freewifi/services/dealservices.dart';
+
+class Dealbloc {
+  Stream<List<Dealdata>> get dealDataListView async* {
+    yield await DealServices.browse();
+  }
+}
